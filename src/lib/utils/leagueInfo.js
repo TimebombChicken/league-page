463f341f70eca67d3f37e9b299bf93dad46492ec
TimebombChicken <90732595+TimebombChicken@users.noreply.github.com>
2021-09-14 17:13:21 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -28,7 +28,7 @@ export const managers = [
       "location": "SoCal", // (optional)
       "bio": "Lorem ipsum...",
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 20XX, // (optional) when did the manager start playing fantasy football
+//      "fantasyStart": 2000, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "njy", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
